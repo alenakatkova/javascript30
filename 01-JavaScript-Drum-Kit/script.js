a5b1a957a4703d004f1d16c578664d0ce4518af2
Alena Katkova <katkova.web@gmail.com>
2020-07-27 16:03:53 +0200

    rewind audio when key is pressed

--- 01-JavaScript-Drum-Kit/script.js
+++ 01-JavaScript-Drum-Kit/script.js
@@ -1,15 +1,24 @@
+/**
+ * @param {string} keyPressedValue
+ */
+const playAudio = (keyPressedValue) => {
+  const audio = document.querySelector(`audio[data-key="${keyPressedValue}"]`);
+  audio.currentTime = 0;
+  audio.play();
+};
+
 // get from the layout all keys which we use in the drum kit
-let keys = Array.prototype.slice.call(document.querySelectorAll(".key"))
+const keys = Array.prototype.slice.call(document.querySelectorAll(".key"))
     .map(button => button.dataset.key);
 
 // add class "playing" on keypress and remove it after transition ends
 document.addEventListener("keydown", (e) => {
-  let keyPressedValue = e.key.toLowerCase();
-  let keyPressedElement = document.querySelector(`div[data-key="${keyPressedValue}"]`);
+  const keyPressedValue = e.key.toLowerCase();
+  const keyPressedElement = document.querySelector(`div[data-key="${keyPressedValue}"]`);
 
   if (keys.indexOf(keyPressedValue) > -1) {
     keyPressedElement.classList.add("playing");
-    document.querySelector(`audio[data-key="${keyPressedValue}"]`).play();
+    playAudio(keyPressedValue);
 
     const removeActiveClassHandler = () => {
       keyPressedElement.classList.remove("playing");
