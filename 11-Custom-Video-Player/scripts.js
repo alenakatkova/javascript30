ebd26c3dbdfd9155cb8e8674c09f8643b7d2016e
Alena Katkova <katkova.web@gmail.com>
2021-09-22 18:31:21 +0200

    change playback rate on input-range

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -6,6 +6,7 @@ const symbols = {
 const video = document.querySelector(".viewer");
 const toggle = document.querySelector(".toggle");
 const volume = document.querySelector(".volume");
+const playbackRate = document.querySelector(".playbackRate")
 
 let status = "paused";
 
@@ -23,9 +24,13 @@ const playOrPause = () => {
 
 toggle.innerHTML = symbols.play;
 video.volume = volume.value;
+video.playbackRate = playbackRate.value;
 
 video.addEventListener("click", playOrPause);
 toggle.addEventListener("click", playOrPause);
 volume.addEventListener("input", () => {
   video.volume = volume.value;
+});
+playbackRate.addEventListener("input", () => {
+  video.playbackRate = playbackRate.value;
 });
