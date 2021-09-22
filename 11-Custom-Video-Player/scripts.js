b9672df0531f215eeb2a0c2ef33a46d6243d4c7a
Alena Katkova <katkova.web@gmail.com>
2021-09-22 18:21:55 +0200

    play and pause on toggle click

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -1,14 +1,27 @@
+const symbols = {
+  play: "&#9654;",
+  pause: "&#9616;&#9616;"
+};
+
 const video = document.querySelector(".viewer");
+const toggle = document.querySelector(".toggle");
+
+
+
 let status = "paused";
 
 const playOrPause = () => {
   if (status === "paused") {
     video.play();
     status = "played";
+    toggle.innerHTML = symbols.pause;
   } else {
     video.pause();
     status = "paused";
+    toggle.innerHTML = symbols.play;
   }
 };
 
+toggle.innerHTML = symbols.play;
 video.addEventListener("click", playOrPause);
+toggle.addEventListener("click", playOrPause);
