a7d96bf5ae5d43a182a32fefe0d0275546a9ad6c
Alena Katkova <katkova.web@gmail.com>
2021-10-15 10:24:52 +0200

    show video from camera in video el and in canvas el

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,30 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const constraints = {
+  audio: false,
+  video: {
+    width: 1280,
+    height: 720
+  }
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((mediaStream) => {
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = (e) => {
+        video.play();
+      };
+    })
+    .catch((err) => {
+      console.log(err.name + ": " + err.message);
+    });
+
+video.addEventListener('play', () => {
+  const step = () => {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(step)
+  };
+  window.requestAnimationFrame(step);
+});
