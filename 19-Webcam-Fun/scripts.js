bd0fec3da77e9c58c278b05f7396cb60ae5f392d
Alena Katkova <katkova.web@gmail.com>
2021-10-15 14:56:31 +0200

    delete pixels on rgb settings change

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -4,6 +4,34 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+const isInRange = (value, min, max) => {
+  return value >= min && value <= max;
+};
+
+const getRgbRangesFromInputs = () => {
+  return [...document.querySelectorAll('input[type="range"]')].reduce((acc, curr) => {
+    acc[curr.name] = curr.value;
+    return acc;
+  }, {});
+};
+
+const deletePixelsInRange = (pixels, ranges) => {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    const red = pixels.data[i];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+
+    if (
+        isInRange(red, ranges.rmin, ranges.rmax)
+        && isInRange(green, ranges.gmin, ranges.gmax)
+        && isInRange(blue, ranges.bmin, ranges.bmax)
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
+};
+
 const constraints = {
   audio: false,
   video: {
@@ -24,8 +52,16 @@ navigator.mediaDevices.getUserMedia(constraints)
     });
 
 video.addEventListener('play', () => {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
   const step = () => {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = deletePixelsInRange(pixels, getRgbRangesFromInputs());
+    ctx.putImageData(pixels, 0, 0);
     window.requestAnimationFrame(step)
   };
   window.requestAnimationFrame(step);
